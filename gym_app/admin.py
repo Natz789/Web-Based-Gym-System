3586567e0a1e2e9ddf8cf606b399f311abe13a7d
Natz789 <145219439+Natz789@users.noreply.github.com>
2025-10-20 17:44:46 +0800

    Add files via upload
    Fix Nav Bar position

--- gym_app/admin.py
+++ gym_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, MembershipPlan, FlexibleAccess, UserMembership, Payment, WalkInPayment, Analytics
+from .models import User, MembershipPlan, FlexibleAccess, UserMembership, Payment, WalkInPayment, Analytics, AuditLog
 
 
 @admin.register(User)
